5427db9990e91327a85ec237f8d5cb7e7b6c07a9
ksherlock <ksherlock@aa027e90-d47c-11dd-86d7-074df07e0730>
2011-02-19 18:07:04 +0000

    new branch to integrate BlockDevice, BlockCache
    git-svn-id: https://profuse.googlecode.com/svn/branches/profuse_interim@333 aa027e90-d47c-11dd-86d7-074df07e0730

---
+++ Endian/IOBuffer.h
@@ -0,0 +1,15 @@
+#ifndef __IOBUFFER_H__
+#define __IOBUFFER_H__
+
+#include <Endian/Endian.h>
+#include <cstring>
+
+namespace LittleEndian {
+#include "IOBuffer.cpp.h" 
+}
+
+namespace BigEndian {
+#include "IOBuffer.cpp.h"
+}
+
+#endif
